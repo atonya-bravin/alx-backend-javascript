44ae95d34bf4c8fbb14008c461f0fd49b8a16970
= <=>
2023-06-28 16:32:48 +0000

    using the const keyword to make the variables unchangeable

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,5 +2,11 @@ export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
+  // eslint-disable-next-line
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
   return [task, task2];
 } 
