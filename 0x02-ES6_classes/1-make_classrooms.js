ede3b5a5856ef20345948eff6291f0e0040bb037
= <=>
2023-06-29 15:01:30 +0000

    implementation of a  function named initializeRooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,15 @@
 import ClassRoom from './0-classroom.js';
 
-function initializeRooms(){
+function initializeRooms() {
   const roomSizes = [19, 20, 34];
   const rooms = [];
 
-  for (const roomSize of roomSizes) {
-    const room = new ClassRoom(roomSize);
+  for (const size of roomSizes) {
+    const room = new ClassRoom(size);
     rooms.push(room);
   }
 
   return rooms;
-	}
+}
 
 export default initializeRooms;
