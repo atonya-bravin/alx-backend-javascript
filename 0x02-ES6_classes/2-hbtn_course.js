f7f5a8721dcb320d65c22b89a78a3a52e6a4aae3
= <=>
2023-06-29 15:07:16 +0000

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = typeof name === 'string' ? name : '';
+    this._length = typeof length === 'number' ? length : 0;
+    this._students = Array.isArray(students) ? students : [];
+  }
+
+  // Getter and Setter for the 'name' attribute
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Invalid value for name. String expected.');
+    }
+  }
+
+  // Getter and Setter for the 'length' attribute
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new Error('Invalid value for length. Number expected.');
+    }
+  }
+
+  // Getter and Setter for the 'students' attribute
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
+      throw new Error('Invalid value for students. Array expected.');
+    }
+  }
+}
