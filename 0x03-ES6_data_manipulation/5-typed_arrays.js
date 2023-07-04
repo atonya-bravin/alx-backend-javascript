6d5f5dc3c50c414ef8a654deac4403a4b1b81a0b
= <=>
2023-07-04 14:19:51 +0000

    Contains the function that returns a new ArrayBuffer with an Int8 value at a specific position.

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(array_length, position, value) {
+  const buffer = new ArrayBuffer(array_length);
+  const int8_array = new Int8Array(buffer);
+
+  if (position < 0 || position >= array_length) {
+    throw new Error('Position outside range');
+  }
+
+  int8_array[position] = value;
+
+  return buffer;
+}
