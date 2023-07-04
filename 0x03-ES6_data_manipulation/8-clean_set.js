83e55ee3185a4f551c2d8050fe21bc459f068a5c
= <=>
2023-07-04 15:21:25 +0000

    function named cleanSet that returns a string of all the set values that start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,9 @@
 export default function cleanSet(set, startString) {
-  const result = [];
-
-  set.forEach(value => {
-    if (value.startsWith(startString)) {
-      result.push(value.substring(startString.length));
-    }
-  });
-
+  let result;
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  result = Array.from(set).filter((word) => typeof word === 'string' && word.startsWith(startString));
+  result = result.map((word) => word.slice(startString.length));
   return result.join('-');
 }
