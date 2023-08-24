dcf93a871ab490cd702dd890dc8d203f9b9c52ee
= <=>
2023-08-24 18:49:00 +0000

    5-paymnet.js test file

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    // runs before each test in this block
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // runs after each test in this block
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
+
+  it('checks output of sendPaymentRequestToApi with 100 and 20 as args', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
