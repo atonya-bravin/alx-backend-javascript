50840825561ccbc921fdc31b15f68275e60d99df
= <=>
2023-08-24 19:31:23 +0000

    test for 10 and 10

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,28 +1,31 @@
+const mocha = require('mocha');
 const { expect } = require('chai');
 const sinon = require('sinon');
 
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let consoleSpy;
+  let consSpy;
 
   beforeEach(() => {
     // runs before each test in this block
-    consoleSpy = sinon.spy(console, 'log');
+    consSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
     // runs after each test in this block
-    expect(consoleSpy.calledOnce).to.be.true;
-    consoleSpy.restore();
+    consSpy.restore();
   });
 
   it('checks output of sendPaymentRequestToApi with 100 and 20 as args', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
-  });
-  it('checks output of sendPaymentRequestToApi with 10 and 10 as args', () => {
-    sendPaymentRequestToApi(10, 10);
-    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consSpy.calledOnce).to.be.true;
   });
 
+  it('checks output of sendPaymentRequestToApi with 10 and 10 as args', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consSpy.calledOnce).to.be.true;
+  });
+});
