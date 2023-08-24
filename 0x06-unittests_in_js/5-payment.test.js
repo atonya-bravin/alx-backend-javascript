1e09db2684531709987e7cd1e3c5e13431f85d4f
= <=>
2023-08-24 19:25:57 +0000

    test for 10 and 10

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -19,10 +19,10 @@ describe('sendPaymentRequestToApi', () => {
 
   it('checks output of sendPaymentRequestToApi with 100 and 20 as args', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
   });
   it('checks output of sendPaymentRequestToApi with 10 and 10 as args', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
   });
 
